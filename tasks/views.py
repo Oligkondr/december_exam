252bba29a3093e833ac3ec6b6843b59507886f97
Oleg <oligkondr@mail.ru>
2025-12-26 10:03:38 +0300

    Finish

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Task
+from .serializers import TaskSerializer
+
 
 # Create your views here.
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
